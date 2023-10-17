bfd4e6717a6c2443dc2350760cfc17c313e20b2c
oconner9 <oconner9@tcd.ie>
2023-10-17 13:08:23 +0100

    Render env every SHOW EVERY episodes

--- StableBaselines3Tutorial/Q_Table.py
+++ StableBaselines3Tutorial/Q_Table.py
@@ -9,42 +9,49 @@
 import gymnasium as gym
 import numpy as np
 
-
-env = gym.make('MountainCar-v0', render_mode='human')
-state, info = env.reset()
-
 # Hyperparameters
 LEARNING_RATE = 0.1 # How much we update our Q-values at each iteration
 DISCOUNT = 0.95 # How much we value future rewards over current rewards
-EPISODES = 25000 # How many episodes we want to run
+EPISODES = 10000000 # How many episodes we want to run
+SHOW_EVERY = 10000000 # How often we want to render the environment
 
-DISCRETE_OS_SIZE = [20] * len(env.observation_space.high)
-discrete_os_win_size = (env.observation_space.high - env.observation_space.low) / DISCRETE_OS_SIZE
+DISCRETE_OS_SIZE = [20] * len(gym.make('MountainCar-v0').observation_space.high)
+discrete_os_win_size = (gym.make('MountainCar-v0').observation_space.high - gym.make('MountainCar-v0').observation_space.low) / DISCRETE_OS_SIZE
 
-# The Q-values contains the Q-values for every state-action pair.
-# A Q-value is the expected future reward for a given state-action pair.
-q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
+q_table = np.random.uniform(low=-2, high=0, size=(DISCRETE_OS_SIZE + [gym.make('MountainCar-v0').action_space.n]))
 
 def get_discrete_state(state):
-    diff = state - env.observation_space.low
+    diff = state - gym.make('MountainCar-v0').observation_space.low
     division_result = diff / discrete_os_win_size
     discrete_state = division_result.astype(np.int32)
     return tuple(discrete_state.astype(np.int32))
 
-discrete_state = get_discrete_state(state)
+for episode in range(EPISODES):
+    render = episode % SHOW_EVERY == 0
+    env = gym.make('MountainCar-v0', render_mode='human' if render else None)
+    state, info = env.reset()
+    discrete_state = get_discrete_state(state)
 
-print(discrete_state)
+    if render:
+        print(episode)
 
-# # Resets the environment to an initial state, required before calling step. 
-# # Returns the first agent observation for an episode and information, i.e. metrics, debug info.
-# observation, info = env.reset()
-# 
-# while True:
-#     action = 2 
-#     observation, reward, terminated, truncated, info = env.step(action)
-#     print(observation)
-# 
-# # When the end of an episode is reached (terminated or truncated), it is necessary 
-# # to call reset() to reset this environment’s state for the next episode.
-# 
-# env.close()
+    while True:
+        action = np.argmax(q_table[discrete_state])
+        observation, reward, terminated, truncated, _ = env.step(action)  # Modified the unpacking of return values.
+        new_discrete_state = get_discrete_state(observation)
+
+        done = terminated or truncated  # Combining termination and truncation
+
+        if not done:
+            max_future_q = np.max(q_table[new_discrete_state])
+            current_q = q_table[discrete_state + (action, )]
+            new_q = (1 - LEARNING_RATE) * current_q + LEARNING_RATE * (reward + DISCOUNT * max_future_q)
+            q_table[discrete_state + (action, )] = new_q
+
+        elif observation[0] >= env.goal_position:
+            print(f"We made it on episode {episode}")
+            q_table[discrete_state + (action, )] = 0
+
+        discrete_state = new_discrete_state
+
+    env.close()  # Close environment after each episode
